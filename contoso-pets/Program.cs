7c80965bea88cc25f3c379356b8f88b4db24bb05
RossaMania <raclettenberg@gmail.com>
2024-05-15 12:19:44 -0500

    refactor: Update main menu display and input handling in do-while loop.

--- contoso-pets/Program.cs
+++ contoso-pets/Program.cs
@@ -86,24 +86,27 @@ namespace contoso_pets
         ourAnimals[i, 5] = "Personality: " + animalPersonalityDescription;
       }
 
-      // display the top-level menu options
+      do
+      {
+        // display the top-level menu options
+        Console.Clear();
 
-      Console.Clear();
+        Console.WriteLine("Welcome to the Contoso PetFriends app. Your main menu options are:");
+        Console.WriteLine(" 1. List all of our current pet information");
+        Console.WriteLine(" 2. Add a new animal friend to the ourAnimals array");
+        Console.WriteLine(" 3. Ensure animal ages and physical descriptions are complete");
+        Console.WriteLine(" 4. Ensure animal nicknames and personality descriptions are complete");
+        Console.WriteLine(" 5. Edit an animal’s age");
+        Console.WriteLine(" 6. Edit an animal’s personality description");
+        Console.WriteLine(" 7. Display all cats with a specified characteristic");
+        Console.WriteLine(" 8. Display all dogs with a specified characteristic");
+        Console.WriteLine();
+        Console.WriteLine("Enter your selection number (or type Exit to exit the program)");
 
-      Console.WriteLine("Welcome to the Contoso PetFriends app. Your main menu options are:");
-      Console.WriteLine(" 1. List all of our current pet information");
-      Console.WriteLine(" 2. Add a new animal friend to the ourAnimals array");
-      Console.WriteLine(" 3. Ensure animal ages and physical descriptions are complete");
-      Console.WriteLine(" 4. Ensure animal nicknames and personality descriptions are complete");
-      Console.WriteLine(" 5. Edit an animal’s age");
-      Console.WriteLine(" 6. Edit an animal’s personality description");
-      Console.WriteLine(" 7. Display all cats with a specified characteristic");
-      Console.WriteLine(" 8. Display all dogs with a specified characteristic");
-      Console.WriteLine();
-      Console.WriteLine("Enter your selection number (or type Exit to exit the program)");
+        readResult = Console.ReadLine();
+        // pause code execution
 
-      readResult = Console.ReadLine();
-      if (readResult != null)
+        if (readResult != null)
       {
         menuSelection = readResult.ToLower();
       }
@@ -113,6 +116,8 @@ namespace contoso_pets
 
       // pause code execution
       readResult = Console.ReadLine();
+
+      } while (menuSelection != "exit");
 
     }
 
