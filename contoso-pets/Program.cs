dac62eba1a96668e833368b29e7cc53e960f1bb7
RossaMania <raclettenberg@gmail.com>
2024-05-27 21:36:19 -0500

    feat: Implemented user input for dog characteristic search in menu option 8.

--- contoso-pets/Program.cs
+++ contoso-pets/Program.cs
@@ -91,9 +91,10 @@ namespace contoso_pets
         ourAnimals[i, 3] = "Nickname: " + animalNickname;
         ourAnimals[i, 4] = "Physical description: " + animalPhysicalDescription;
         ourAnimals[i, 5] = "Personality: " + animalPersonalityDescription;
-        if (!decimal.TryParse(suggestedDonation, out decimalDonation)){
-        decimalDonation = 45.00m; // if suggestedDonation NOT a number, default to 45.00
-          }
+        if (!decimal.TryParse(suggestedDonation, out decimalDonation))
+        {
+          decimalDonation = 45.00m; // if suggestedDonation NOT a number, default to 45.00
+        }
         ourAnimals[i, 6] = $"Suggested Donation: {decimalDonation:C2}";
       }
 
@@ -445,9 +446,18 @@ namespace contoso_pets
 
           case "8":
             // Display all dogs with a specified characteristic
-            Console.WriteLine(
-                "UNDER CONSTRUCTION - please check back next month to see progress."
-            );
+            string dogCharacteristic = "";
+
+            while (dogCharacteristic == "")
+            {
+              // have the user enter physical characteristics to search for
+              Console.WriteLine($"\nEnter one desired dog characteristics to search for");
+              readResult = Console.ReadLine();
+              if (readResult != null)
+              {
+                dogCharacteristic = readResult.ToLower().Trim();
+              }
+            } // end while
             Console.WriteLine("Press the Enter key to continue.");
             readResult = Console.ReadLine();
             break;
